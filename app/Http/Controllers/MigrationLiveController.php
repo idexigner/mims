deaecd51fb3de82656a8101933c725118f46beda
faraz.a <faraz.a@sybrid.com>
2023-04-26 18:08:49 +0500

    Migration database

--- app/Http/Controllers/MigrationLiveController.php
+++ app/Http/Controllers/MigrationLiveController.php
@@ -20,8 +20,264 @@ class MigrationLiveController extends Controller
         // $this->migrate_manufacturer();
         // $this->migrate_brand();
 
-        
+        // $this->migrate_location();
 
+        // $this->migrate_country();
+        // $this->migrate_state();
+        // $this->migrate_city();
+        // $this->migrate_doctor();
+        // $this->migrate_job();
+        // $this->migrate_news();
+    }
+
+    function migrate_news(){
+        try{
+            DB::connection('mysql')->table('news')->truncate();
+                    // Get the data from the source table
+                    $newss = DB::connection('db_live')->select('SELECT * FROM mims_newsinformation');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($news) {
+                        return [
+                            'news_id' => $news->ID,
+                            'news_title' => $news->Title,
+                            'news_description' => $news->Description,
+                            'news_image' => $news->ImagePath,
+                            'news_publish_date' => $news->PublishDateTime,
+                            'news_unpublish_date' => $news->UnpublishedDateTime,
+                            'news_is_active' => 1,
+                            'news_is_deleted' => $news->IsDeleted,
+                            'news_created_by' => 1,
+                            'news_created_at' => $news->LastUpdate,
+                            'news_updated_by' => 0,
+                            'news_updated_at' => $news->LastUpdate,
+                        ];
+                    }, $newss);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('news')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_job(){
+        try{
+            DB::connection('mysql')->table('job')->truncate();
+                    // Get the data from the source table
+                    $jobs = DB::connection('db_live')->select('SELECT * FROM mims_jobinformation');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($job) {
+
+                        $job_category = $job->JobCategory;
+                        if($job_category == 1){
+                            $job_category = "Pharma";
+                        }else{
+                            $job_category = "Medical";
+                        }
+                        return [
+                            'job_id' => $job->ID,
+                            'job_category' => $job_category,
+                            'job_title' => $job->Title,
+                            'job_description' => $job->Description,
+                            'job_organization' => $job->Organization,
+                            'job_organization_logo' => $job->OrganizationLogo,
+                            'job_position' => $job->Position,
+                            'job_application_deadline' => $job->ApplicationDeadline,
+                            'job_salary' => $job->Salary,
+                            'job_educational_requirement' => $job->EducationalRequirement,
+                            'job_experience_requirement' => $job->ExperienceRequirement,
+                            'job_vacancy' => $job->NumberOfVacancy,
+                            'job_age_limit' => $job->AgeLimit,
+                            'job_location' => $job->Location,
+                            'job_source' => $job->JobSource,
+                            'job_type' => $job->JobType,
+                            'job_employment_type' => $job->EmploymentType,
+                            'job_nature' => $job->JobNature,
+                            'job_application_procedure' => $job->ApplyingProcedure,
+                            'job_publish_date' => $job->PublishDate,
+                            'job_image' => $job->JobCircularImagePath,
+                            'job_is_active' => 1,
+                            'job_is_deleted' => $job->IsDeleted,
+                            'job_created_by' => 1,
+                            'job_created_at' => $job->LastUpdate,
+                            'job_updated_by' => 0,
+                            'job_updated_at' => $job->LastUpdate,
+                        ];
+                    }, $jobs);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('job')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_doctor(){
+        try{
+            DB::connection('mysql')->table('doctor')->truncate();
+                    // Get the data from the source table
+                    $doctors = DB::connection('db_live')->select('SELECT * FROM mims_doctorinformation');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($doctor) {
+                        return [
+                            'doctor_id' => $doctor->ID,
+                            'doctor_name' => $doctor->Name,
+                            'doctor_email' => $doctor->email_id,
+                            'doctor_specialization' => $doctor->Specialization,
+                            'doctor_experience' => $doctor->experiance,
+                            'doctor_achievement' => $doctor->achievement,
+                            'doctor_address' => $doctor->clinic_address,
+                            // 'doctor_name' => $doctor->clinic_line1,
+                            // 'doctor_name' => $doctor->clinic_line2,
+                            'doctor_country_id' => $doctor->clinic_country_id,
+                            'doctor_state_id' => $doctor->clinic_state_id,
+                            'doctor_city_id' => $doctor->clinic_city_id,
+                            'doctor_zip_code' => $doctor->clinic_zipcode,
+                            'doctor_profession_degree' => $doctor->ProfessionDegree,
+                            'doctor_gender' => $doctor->Gender,
+                            'doctor_image' => $doctor->ImagePath,
+                            'doctor_certificate' => $doctor->certificate,
+                            'doctor_bio_notes' => $doctor->bio_notes,
+                            // 'doctor_name' => $doctor->HomeAddressID,
+                            // 'doctor_name' => $doctor->ChamberAddressID,
+                            'doctor_phone_personal' => $doctor->PhoneNo,
+                            'doctor_phone_clinic' => $doctor->clinic_phone,
+                            // 'doctor_name' => $doctor->MobileNo1,
+                            // 'doctor_name' => $doctor->MobileNo2,
+                            // 'doctor_name' => $doctor->MobileNo3,
+                            // 'doctor_name' => $doctor->Hotline,                            
+                            'doctor_is_active' => 1,
+                            'doctor_is_deleted' => $doctor->IsDeleted,
+                            'doctor_created_by' => 1,
+                            'doctor_created_at' => $doctor->LastUpdate,
+                            'doctor_updated_by' => 0,
+                            'doctor_updated_at' => $doctor->LastUpdate,
+                        ];
+                    }, $doctors);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('doctor')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_city(){
+        try{
+            DB::connection('mysql')->table('city')->truncate();
+                    // Get the data from the source table
+                    $citys = DB::connection('db_live')->select('SELECT * FROM mims_city');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($city) {
+                        return [
+                            'city_id' => $city->ID,
+                            'city_name' => $city->Name,
+                            'city_state_id' => $city->StateID,
+                            'city_is_active' => 1,
+                            'city_is_deleted' => $city->IsDeleted,
+                            'city_created_by' => 1,
+                            'city_created_at' => $city->LastUpdate,
+                            'city_updated_by' => 0,
+                            'city_updated_at' => $city->LastUpdate,
+                        ];
+                    }, $citys);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('city')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_state(){
+        try{
+            DB::connection('mysql')->table('state')->truncate();
+                    // Get the data from the source table
+                    $states = DB::connection('db_live')->select('SELECT * FROM mims_state');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($state) {
+                        return [
+                            'state_id' => $state->ID,
+                            'state_name' => $state->Name,
+                            'state_country_id' => $state->CountryID,
+                            'state_is_active' => 1,
+                            'state_is_deleted' => $state->IsDeleted,
+                            'state_created_by' => 1,
+                            'state_created_at' => $state->LastUpdate,
+                            'state_updated_by' => 0,
+                            'state_updated_at' => $state->LastUpdate,
+                        ];
+                    }, $states);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('state')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_country(){
+        try{
+            DB::connection('mysql')->table('country')->truncate();
+                    // Get the data from the source table
+                    $countrys = DB::connection('db_live')->select('SELECT * FROM mims_countryinformation');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($country) {
+                        return [
+                            'country_id' => $country->ID,
+                            'country_name' => $country->Name,
+                            'country_is_active' => 1,
+                            'country_is_deleted' => $country->IsDeleted,
+                            'country_created_by' => 1,
+                            'country_created_at' => $country->LastUpdate,
+                            'country_updated_by' => 0,
+                            'country_updated_at' => $country->LastUpdate,
+                        ];
+                    }, $countrys);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('country')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
+    }
+
+    function migrate_location(){
+        try{
+            DB::connection('mysql')->table('location')->truncate();
+                    // Get the data from the source table
+                    $locations = DB::connection('db_live')->select('SELECT * FROM mims_location');
+
+                    // Map the columns to match the destination table
+                    $mappedData = array_map(function ($location) {
+                        return [
+                            'location_id' => $location->ID,
+                            'location_country_id' => $location->CountryID,
+                            'location_state_id' => $location->StateID,
+                            'location_city_id' => $location->CityID,
+                            'location_address' => $location->Address,
+                            'location_longitude' => $location->Longitude,
+                            'location_latitude' => $location->Latitude,                     
+                            'location_is_active' => 1,
+                            'location_is_deleted' => $location->IsDeleted,
+                            'location_created_by' => 1,
+                            'location_created_at' => $location->LastUpdate,
+                            'location_updated_by' => 0,
+                            'location_updated_at' => $location->LastUpdate,
+                        ];
+                    }, $locations);
+
+            // Insert the data into the destination table
+            DB::connection('mysql')->table('location')->insert($mappedData);
+        }catch(Exception $ex){
+            dd($ex->getMessage());
+        }
     }
 
 
@@ -36,6 +292,18 @@ class MigrationLiveController extends Controller
                         return [
                             'brand_id' => $brand->ID,
                             'brand_name' => $brand->Name,
+                            'brand_type' => $brand->ProductType,
+                            'brand_generic_id' => $brand->GenericID,
+                            'brand_manufacturer_id' => $brand->ManufacturerID,
+                            'brand_dosage_form_id' => $brand->DosageFormID,
+                            'brand_strength_id' => $brand->StrengthID,
+                            'brand_pack_size_id' => $brand->PackSizeID,
+                            'brand_image' => $brand->ImagePath,
+                            'brand_price' => $brand->PriceInBDT,
+                            'brand_is_hightlight' => $brand->IsHighlighted,
+                            'brand_is_new_product' => $brand->IsNewProduct,
+                            'brand_is_new_brand' => $brand->IsNewBrand,
+                            'brand_is_new_presentation' => $brand->IsNewPresentation,
                             'brand_is_active' => $brand->IsActive,
                             'brand_is_deleted' => $brand->IsDeleted,
                             'brand_created_by' => 1,
@@ -63,6 +331,11 @@ class MigrationLiveController extends Controller
                         return [
                             'manufacturer_id' => $manufacturer->ID,
                             'manufacturer_name' => $manufacturer->Name,
+                            'manufacturer_location_id' => $manufacturer->AddressID,
+                            'manufacturer_email' => $manufacturer->EmailID,
+                            'manufacturer_phone' => $manufacturer->PhoneNo,
+                            'manufacturer_mobile' => $manufacturer->MobileNo,
+                            'manufacturer_fax' => $manufacturer->FaxNo,
                             'manufacturer_is_active' => $manufacturer->IsActive,
                             'manufacturer_is_deleted' => $manufacturer->IsDeleted,
                             'manufacturer_created_by' => 1,
@@ -122,7 +395,7 @@ class MigrationLiveController extends Controller
                             'strength_created_by' => 1,
                             'strength_created_at' => $strength->LastUpdate,
                             'strength_updated_by' => 0,
-                            'strength_updated_at' => now(),
+                            'strength_updated_at' => $strength->LastUpdate,
                         ];
                     }, $strengths);
 
